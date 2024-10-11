e80b9cf0ffd71b6433f58a1a3e753c3fa90ccbe3
Khushi-M-0521 <khushim0521@gmail.com>
2024-10-11 13:37:59 +0530

    login_ui

--- eventorchestr8/lib/main.dart
+++ eventorchestr8/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData.light().copyWith(
         colorScheme: lightColorScheme,  
+        scaffoldBackgroundColor: Colors.white
       ),
       darkTheme: ThemeData.dark().copyWith(
         colorScheme: darkColorScheme,  
--- eventorchestr8/lib/screens/login_screen.dart
+++ eventorchestr8/lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:eventorchestr8/widgets/rounded_button.dart';
 import 'package:eventorchestr8/widgets/subtitle_text.dart';
 import 'package:eventorchestr8/widgets/title_text.dart';
@@ -11,21 +13,212 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool switched = false;
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  Country selectedCountry = Country(
+    phoneCode: "91",
+    countryCode: "IN",
+    e164Sc: 0,
+    geographic: true,
+    level: 1,
+    name: "India",
+    example: "9010102030",
+    displayName: "India",
+    displayNameNoCountryCode: "IN",
+    e164Key: "",
+  );
+
+  void swap() {
+    setState(() {
+      switched = !switched;
+    });
+  }
+
+  Widget textButton(String text) {
+    return TextButton(
+      onPressed: swap,
+      child: Text(text),
+    );
+  }
+
+  Widget filledButton(String text) {
+    return FilledButton(
+      onPressed: swap,
+      child: Text(text),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("EventOrchestr8..."),
-              RoundedButton(onPressed: () {},
-              child: const Text("button"),),
-              const TitleText(text: "Verify Your Mobile Number"),
-              const SubtitleText(text: "A Verification Code is sent to (+91) 9010102030"),
-            ],
+    phoneController.selection = TextSelection.fromPosition(
+        TextPosition(offset: phoneController.text.length));
+
+    Widget correctIcon = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 20,
+        width: 20,
+        //padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        margin: const EdgeInsets.all(10),
+        decoration:
+            const BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+        child: const Icon(
+          Icons.done,
+          color: Colors.white,
+          size: 15,
+        ),
+      ),
+    );
+
+    TextField emailField = TextField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (value) {
+        setState(() {
+          emailController.text = value;
+        });
+      },
+      style: const TextStyle(
+        fontWeight: FontWeight.w600,
+      ),
+      decoration: InputDecoration(
+        hintText: "abc@gmail.com",
+        hintStyle: TextStyle(color: Colors.grey[400]),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide: const BorderSide(color: Colors.black12)),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+        prefixIcon: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Icon(Icons.email_outlined),
+        ),
+        suffixIcon:
+            EmailValidator.validate(emailController.text) ? correctIcon : null,
+      ),
+    );
+
+    TextFormField phoneField = TextFormField(
+      controller: phoneController,
+      keyboardType: const TextInputType.numberWithOptions(),
+      onChanged: (value) {
+        setState(() {
+          phoneController.text = value;
+        });
+      },
+      style: const TextStyle(
+        fontWeight: FontWeight.w600,
+      ),
+      decoration: InputDecoration(
+        hintText: selectedCountry.example,
+        hintStyle: TextStyle(color: Colors.grey[400]),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide: const BorderSide(color: Colors.black12)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary)),
+        prefixIcon: Container(
+          padding: const EdgeInsets.all(15.0),
+          child: InkWell(
+            onTap: () {
+              showCountryPicker(
+                context: context,
+                countryListTheme: const CountryListThemeData(
+                  bottomSheetHeight: 500,
+                ),
+                onSelect: (country) {
+                  setState(() {
+                    selectedCountry = country;
+                  });
+                },
+              );
+            },
+            child: Text(
+              "${selectedCountry.flagEmoji} +${selectedCountry.phoneCode}",
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
-      );
+        suffixIcon:
+            phoneController.text.length == selectedCountry.example.length
+                ? correctIcon
+                : null,
+      ),
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TitleText(text: "Login Account"),
+                  const SubtitleText(text: "Hello, Welcome to EventOrchetr8"),
+                  const SizedBox(height: 30),
+                  Container(
+                    width: double.infinity,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.grey[200],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        children: [
+                          switched
+                              ? textButton("EMAIL ID")
+                              : filledButton("EMAIL ID"),
+                          const Spacer(),
+                          !switched
+                              ? textButton("PHONE NUMBER")
+                              : filledButton("PHONE NUMBER"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: !switched
+                        ? const Text("Email Address")
+                        : const Text("Phone Number"),
+                  ),
+                  !switched ? emailField : phoneField,
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                      onPressed: () {},
+                      child: const Text("VERIFY"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SubtitleText(text: "Already have an Account?"),
+                TextButton(onPressed: () {}, child: const Text("Sign in"))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
